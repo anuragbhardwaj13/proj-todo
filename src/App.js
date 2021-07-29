f357a0fe8891cc50b201b18e6b6ea226d83951a0
Anurag Bhardwaj <65033601+anuragbhardwaj13@users.noreply.github.com>
2021-07-29 16:42:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import List from "./List";
-import Alert from "./Alert";
-
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem("list")));
+  } else {
+    return [];
+  }
+};
 function App() {
-  return <h1>Todo List</h1>;
+  const [task, setTask] = useState("");
+  const [list, setList] = useState(getLocalStorage);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!task) {
+      alert("Invalid Input");
+    } else if (task) {
+      const newItem = { id: new Date().getTime().toString(), title: task };
+      setList([...list, newItem]);
+      setTask("");
+    }
+  };
+  const clearList = () => {
+    setList([]);
+  };
+  const removeTask = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+  return (
+    <section className="section-center">
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>Todo List</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Enter your Tasks here"
+            className="input"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            Add
+          </button>
+        </div>
+      </form>
+      <div className="container">
+        <List items={list} removeTask={removeTask}></List>
+        <button type="button" className="clear-btn" onClick={clearList}>
+          Clear
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = () => {
-  return <h1>List</h1>;
+const List = ({ items, removeTask }) => {
+  return (
+    <div className="list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="item">
+            <p className="title">{title}</p>
+            <button
+              type="button"
+              className="delete-btn"
+              onClick={() => removeTask(id)}
+            >
+              <FaTrash></FaTrash>
+            </button>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default List;
