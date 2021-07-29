2a8c67a3e3cb39075d84849e1cce80c6e39c5c4f
Anurag Bhardwaj <65033601+anuragbhardwaj13@users.noreply.github.com>
2021-07-29 16:48:59 +0530

    Updates

---
+++ precache-manifest.fdc457579538d163864bd29fd0bfafed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef152bb1ae23c4c65a12d77491e88b0a",
+    "url": "/proj-todo/index.html"
+  },
+  {
+    "revision": "c803297454661add0989",
+    "url": "/proj-todo/static/css/main.ad624072.chunk.css"
+  },
+  {
+    "revision": "f5ee5b0722f713e6c018",
+    "url": "/proj-todo/static/js/2.ebe7a71c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proj-todo/static/js/2.ebe7a71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c803297454661add0989",
+    "url": "/proj-todo/static/js/main.7154936b.chunk.js"
+  },
+  {
+    "revision": "aee37326149724e4ff68",
+    "url": "/proj-todo/static/js/runtime-main.36634bcd.js"
+  }
+]);
